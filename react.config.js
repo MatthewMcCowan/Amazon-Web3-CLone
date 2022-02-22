175c28e73b0fd193afdcd1e8ddba0d572e6c374c
matthew mccowan <robinware77@gmail.com>
2022-02-22 00:35:56 -0600

    someone help me

---
+++ react.config.js
@@ -0,0 +1,11 @@
+export function webpack(
+  config,
+  { buildId, dev, isServer, defaultLoaders, webpack }
+) {
+  config.ignoreWarnings = [
+    {
+      message: /(magic-sdk|@walletconnect\/web3-provider|@web3auth\/web3auth)/,
+    },
+  ];
+  return config;
+}
